d1daeab195e5fa867e873f8ec2fd83371f5978c4
TuiTenTruong <truong0795432149@gmail.com>
2026-03-15 15:35:53 +0700

    init project

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FavoritesScreen extends StatefulWidget {
+  const FavoritesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FavoritesScreen> createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends State<FavoritesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Favorites')),
+      body: const Center(child: Text('Favorites Screen')),
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,8 @@
+// Export all screens
+export 'home_screen.dart';
+export 'profile_screen.dart';
+export 'settings_screen.dart';
+export 'detail_screen.dart';
+export 'search_screen.dart';
+export 'favorites_screen.dart';
+export 'notification_screen.dart';
